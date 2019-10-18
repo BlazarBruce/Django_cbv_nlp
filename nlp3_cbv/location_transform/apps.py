68cdeb751fbeea115a5fddd2a71679c384c44ce0
Bruce <50982818+blazarbruce@users.noreply.github.com>
2019-10-18 15:09:16 +0800

    创建nlp_web、student_sys、location_transform app

---
+++ nlp3_cbv/location_transform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LocationTransformConfig(AppConfig):
+    name = 'location_transform'
---
+++ nlp3_cbv/student_sys/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentSysConfig(AppConfig):
+    name = 'student_sys'
